7030a25db7f9f6c7b5d879f798630ff136fa7567
Carl Lundin <clundin@google.com>
2025-04-09 13:59:15 -0700

    Refactor code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
+use anyhow::Result;
 use chrono::prelude::*;
-use rand::Rng;
 use clap::Parser;
-use octocrab::auth::create_jwt;
-
-use std::env;
-use std::process::Command;
-
+use rand::Rng;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Stage {
@@ -22,7 +18,10 @@ impl std::str::FromStr for Stage {
             "carl" => Ok(Stage::Carl),
             "staging" => Ok(Stage::Staging),
             "prod" => Ok(Stage::Prod),
-            _ => Err(format!("Invalid stage: '{}'. Must be one of 'carl', 'staging', or 'prod'.", s)),
+            _ => Err(format!(
+                "Invalid stage: '{}'. Must be one of 'carl', 'staging', or 'prod'.",
+                s
+            )),
         }
     }
 }
@@ -42,8 +41,56 @@ impl std::str::FromStr for FpgaTarget {
             "zcu104-nightly" => Ok(Self::Zcu104Nightly),
             "zcu104" => Ok(Self::Zcu104),
             "vck190" => Ok(Self::Vck190),
-            _ => Err(format!("Invalid fpga: '{}'. Must be one of 'zcu104', 'zcu104-nightly' or 'vck190'.", s)),
+            _ => Err(format!(
+                "Invalid fpga: '{}'. Must be one of 'zcu104', 'zcu104-nightly' or 'vck190'.",
+                s
+            )),
         }
+    }
+}
+
+/// New type for self-hosted runner labels
+struct RunnerLabels(Vec<String>);
+
+impl From<FpgaTarget> for RunnerLabels {
+    fn from(value: FpgaTarget) -> Self {
+        let inner = match value {
+            FpgaTarget::Zcu104 => vec!["caliptra-fpga".to_string()],
+            FpgaTarget::Zcu104Nightly => {
+                vec!["caliptra-fpga".to_string(), "caliptra-fpga-nightly".to_string()]
+            }
+            FpgaTarget::Vck190 => vec!["vck190".to_string()],
+        };
+        Self(inner)
+    }
+}
+
+/// New type for the runner name
+/// Each runner name uses 16 random hexadecimal's to de-duplicate, in case a runner crashes and
+/// isn't cleaned up.
+struct RunnerName(String);
+impl RunnerName {
+    /// Type is the FPGA board type.
+    /// Identifier is a unique number to differentiate boards running in the CI.
+    /// Location is the physical location of the runner, for example "kir".
+    fn new(fpga_type: FpgaTarget, identifier: &str, location: &str) -> Self {
+        let board_type = match fpga_type {
+            FpgaTarget::Zcu104 | FpgaTarget::Zcu104Nightly => "caliptra-fpga",
+            FpgaTarget::Vck190 => "vck190",
+        };
+
+        let now = Local::now();
+        let current_date = now.date_naive().format("%Y-%m-%d").to_string();
+
+        // Generate a random hexadecimal postfix
+        let mut rng = rand::rng();
+        let rand_postfix: String = (0..16)
+            .map(|_| format!("{:X}", rng.random::<u8>() % 16))
+            .collect();
+
+        let runner_name =
+            format!("{board_type}-{location}-{identifier}-{rand_postfix}-{current_date}",);
+        Self(runner_name)
     }
 }
 
@@ -56,118 +103,104 @@ struct Args {
     fpga_target: FpgaTarget,
     #[clap(short = 'i', long, value_name = "FPGA_IDENTIFIER")]
     fpga_identifier: String,
+    #[clap(short = 'l', long, value_name = "LOCATION")]
+    location: String,
+    #[clap(short, long, value_name = "KEY_PATH")]
+    key_path: String,
+}
+
+struct CaliptraCiInfo {
+    github_app_id: u64,
+    github_installation_id: u64,
+    github_org_name: String,
+    key_path: String,
+}
+
+impl From<Args> for CaliptraCiInfo {
+    fn from(value: Args) -> Self {
+        match value.stage {
+            Stage::Carl => {
+                // Set environment variables for carl
+                CaliptraCiInfo {
+                    github_app_id: 1160975,
+                    github_installation_id: 61798278,
+                    github_org_name: "clundin25-testorg".to_string(),
+                    key_path: value.key_path,
+                }
+            }
+            Stage::Staging => {
+                // TODO: Set environment variables for staging
+                todo!("TODO: Set environment variables for staging");
+            }
+            Stage::Prod => CaliptraCiInfo {
+                github_app_id: 379559,
+                github_installation_id: 40993215,
+                github_org_name: "chipsalliance".to_string(),
+                key_path: value.key_path,
+            },
+        }
+    }
+}
+
+struct OctocrabWrapper {
+    github_org_name: String,
+    octocrab: octocrab::Octocrab,
+}
+
+impl OctocrabWrapper {
+    fn new(info: &CaliptraCiInfo) -> Result<Self> {
+        let github_org_name = info.github_org_name.clone();
+        let key = jsonwebtoken::EncodingKey::from_rsa_pem(&std::fs::read(info.key_path.clone())?)?;
+
+        let octocrab = octocrab::Octocrab::builder()
+            .app(info.github_app_id.into(), key)
+            .build()?;
+        // We already know the installation ID so we can optimistically add it to the `Octocrab`
+        // object.
+        let octocrab = octocrab.installation(info.github_installation_id.into())?;
+        Ok(Self {
+            github_org_name,
+            octocrab,
+        })
+    }
+
+    async fn runner_jit_token(&self, name: RunnerName, labels: RunnerLabels) -> Result<String> {
+        // For Caliptra we only use one runner group.
+        let default_runner_group = 1;
+
+        let token = self
+            .octocrab
+            .actions()
+            .create_org_jit_runner_config(
+                self.github_org_name.clone(),
+                name.0,
+                default_runner_group.into(),
+                labels.0,
+            )
+            .send()
+            .await?;
+
+        Ok(token.encoded_jit_config)
+    }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let args = Args::parse();
-    let stage = args.stage;
+    eprintln!("Running for stage: {:?}", args.stage);
 
-    eprintln!("Running for stage: {:?}", stage);
+    let name = RunnerName::new(args.fpga_target, &args.fpga_identifier, &args.location);
+    let labels = RunnerLabels::from(args.fpga_target);
+    let github = OctocrabWrapper::new(&args.into())?;
 
-    let mut github_app_id: Option<u64> = None;
-    let mut github_installation_id: Option<u64> = None;
-
-    match stage {
-        Stage::Carl => {
-            // Set environment variables for carl
-            env::set_var("GCP_ZONE", "us-central-1");
-            env::set_var("GCP_PROJECT", "carl-caliptra-github-ci");
-            env::set_var("GITHUB_ORG", "clundin25-testorg");
-            github_app_id = Some(1160975);
-            github_installation_id = Some(61798278);
+    match github.runner_jit_token(name, labels).await {
+        Ok(jit_config) => {
+            println!("{jit_config}");
+            std::process::exit(0);
         }
-        Stage::Staging => {
-            // TODO: Set environment variables for staging
-            eprintln!("TODO: Set environment variables for staging");
-        }
-        Stage::Prod => {
-            // TODO: Set environment variables for prod
-            env::set_var("GCP_ZONE", "us-central-1");
-            env::set_var("GCP_PROJECT", "caliptra-github-ci");
-            env::set_var("GITHUB_ORG", "chipsalliance");
-            github_app_id = Some(379559);
-            github_installation_id = Some(40993215);
+        Err(e) => {
+            eprintln!("Failed to create runner config due to {:}", e);
+            std::process::exit(1);
         }
     }
-
-    let fpga = args.fpga_target;
-    let fpga_target = match fpga {
-        FpgaTarget::Zcu104Nightly | FpgaTarget::Zcu104 => "caliptra-fpga",
-        FpgaTarget::Vck190 => "vck190",
-    };
-    let fpga_label = match fpga {
-        FpgaTarget::Zcu104 => "caliptra-fpga",
-        FpgaTarget::Zcu104Nightly => "caliptra-fpga,caliptra-fpga-nightly",
-        FpgaTarget::Vck190 => "vck190",
-    };
-
-    // Generate a random hexadecimal postfix
-    let mut rng = rand::rng();
-    let rand_postfix: String = (0..16)
-        .map(|_| format!("{:X}", rng.gen::<u8>() % 16))
-        .collect();
-
-    let now = Local::now();
-    let current_date = now.date_naive().format("%Y-%m-%d").to_string();
-
-
-    let fpga_identifier = args.fpga_identifier;
-    let rtool_path = "/usr/local/google/home/clundin/code/caliptra-sw/ci-tools/github-runner/cmd/rtool/rtool";
-    let jitconfig_arg = "jitconfig";
-    let final_arg = format!("{}-kir-{}-{}-{}", fpga_target, fpga_identifier,rand_postfix, current_date);
-
-
-    //let mut command = Command::new(rtool_path);
-    //command.arg(jitconfig_arg)
-    //    .arg(fpga_label)
-    //    .arg(github_app_id.unwrap())
-    //    .arg(github_installation_id.unwrap())
-    //    .arg(final_arg);
-    let app_id = octocrab::models::AppId::from(github_app_id.unwrap());
-    let key = jsonwebtoken::EncodingKey::from_rsa_pem(&std::fs::read("/etc/secrets/caliptra-gce-ci-github-private-key-pem/carl").unwrap()).unwrap();
-    let octocrab = octocrab::Octocrab::builder().app(app_id, key).build().unwrap();
-    let octocrab = octocrab.installation(github_installation_id.unwrap().into()).unwrap();
-    let jit_config = octocrab
-        .actions()
-        .create_org_jit_runner_config(
-            "clundin25-testorg",
-            "octocrab-test",
-            1.into(),
-            ["octocrab".into()],
-        )
-        .send()
-        .await.unwrap();
-// jit_config.encoded_jit_config contains the base64-encoded runner configuration
-    println!("{:?}", jit_config.encoded_jit_config);
-
-    // Execute the command and handle the output
-    //match command.status() {
-    //    Ok(status) => {
-    //        if status.success() {
-    //            eprintln!("rtool command executed successfully.");
-    //        } else {
-    //            eprintln!("Error: rtool command failed with status {:?}", status);
-    //        }
-    //    }
-    //    Err(e) => {
-    //        eprintln!("Error executing rtool command: {}", e);
-    //    }
-    //}
-
-    // If you need the output of the command, you can use the following:
-    //match command.output() {
-    //    Ok(output) => {
-    //        if output.status.success() {
-    //            println!("rtool output:\n{}", String::from_utf8_lossy(&output.stdout));
-    //            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-    //        } else {
-    //            eprintln!("Error: rtool command failed with status {:?}", output.status);
-    //            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
-    //        }
-    //    }
-    //    Err(e) => {
-    //        eprintln!("Error executing rtool command: {}", e);
-    //    }
-    //}
 }
